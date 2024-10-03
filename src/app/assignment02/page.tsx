789dbe92b70fd0ec98992b5f735d82ed13ccd1c8
ghulamhasnain007 <ghasnain668@gmail.com>
2024-10-03 09:13:14 +0500

    debug-deployment-error

--- src/app/assignment02/page.tsx
+++ src/app/assignment02/page.tsx
@@ -8,8 +8,8 @@ function Profile() {
        <div className='flex justify-center items-center'>
         <div className='p-5 text-center pt-10 space-y-3'> 
             <h3 className='text-2xl'>HELLO THERE!</h3>
-            <h2 className='text-3xl font-semibold'>I'm GHULAM HASNAIN</h2>
-            <h1 className='text-4xl font-bold'>MERN Stack Developer</h1>
+            <h2 className='text-3xl font-semibold'>I&apos;m GHULAM HASNAIN</h2>
+            <h1 className='text-4xl font-bold'>Next JS Developer</h1>
         </div>
        </div>
         <div className='w-[500px]'>
